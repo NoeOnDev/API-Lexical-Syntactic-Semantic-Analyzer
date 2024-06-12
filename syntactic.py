bcba7850868c1cd2c45e0249feba3e8f504103af
NoeOnDev <alxg5516@gmail.com>
2024-06-12 00:23:56 -0600

    feat: Update syntactic.py to improve error handling
    The code changes in syntactic.py include adding a global variable `error_message` to store syntax error messages and modifying the `p_error` function to update the `error_message` variable with the appropriate error message. This improves the error handling in the module by providing more descriptive error messages for syntax errors encountered during parsing.

--- syntactic.py
+++ syntactic.py
@@ -1,6 +1,8 @@
 import ply.yacc as yacc
 from lexer import tokens
 
+error_message = None
+
 def p_program(p):
     '''program : FOR LPAREN declaration SEMICOLON condition SEMICOLON iteration RPAREN block'''
     p[0] = ('for_loop', p[3], p[5], p[7], p[9])
@@ -26,9 +28,18 @@ def p_statement(p):
     p[0] = ('statement', p[1], p[3])
 
 def p_error(p):
-    print(f"Syntax error at '{p.value}'")
+    global error_message
+    if p is None:
+        error_message = "Syntax error at EOF"
+    else:
+        error_message = f"Syntax error at '{p.value}'"
 
 parser = yacc.yacc()
 
 def parse_code(code):
-    return parser.parse(code)
+    global error_message
+    result = parser.parse(code)
+    if result is None:
+        return {"error": error_message}
+    else:
+        return {"success": "El análisis sintáctico se completó con éxito. La estructura del ciclo 'for' es correcta.", "result": result}
